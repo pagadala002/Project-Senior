ad8720990bd54ff309bb71ffd4e153b8dd1b8895
Nagadeepak Appalaneni <nappalaneni@ts22253.localdomain>
2024-11-11 22:40:16 -0500

    Manual deploy to GitHub Pages

---
+++ precache-manifest.cda326b146eb9d673473caeacdfbdfc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b2e36d505efda2e908e084238e4e26",
+    "url": "/Project-Senior/index.html"
+  },
+  {
+    "revision": "56ce4043a71fab5fdb1a",
+    "url": "/Project-Senior/static/css/main.8f42e603.chunk.css"
+  },
+  {
+    "revision": "d2f4b7b5d34ca51ffb8d",
+    "url": "/Project-Senior/static/js/2.2b920929.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/Project-Senior/static/js/2.2b920929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ce4043a71fab5fdb1a",
+    "url": "/Project-Senior/static/js/main.375b77bf.chunk.js"
+  },
+  {
+    "revision": "93f51e66fe851ab6d625",
+    "url": "/Project-Senior/static/js/runtime-main.6c0b5e25.js"
+  }
+]);
